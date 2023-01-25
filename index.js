c5a3b12eb7706f8d8c30a864071785f848f61159
Silvio Pina <silviopina94@gmail.com>
2023-01-25 17:50:24 -0500

    added 16 by 16 grid

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-function createSquares() {
-  for (let i = 0; i < 20; i++) {
+function createSquares(squares) {
+  for (let i = 0; i < squares; i++) {
     let square = document.createElement("div");
-    square.className = "board";
-    square.style.width = "50px";
-    square.style.height = "50px";
-    square.style.backgroundColor = "aqua";
+    //square.className = "square";
+
+    square.style.cssText = "height: 30px; width: 30px; border: 1px solid aqua";
+
     document.querySelector(".container").appendChild(square);
+
+    square.addEventListener(
+      "mouseover",
+      (e) => (e.target.style.backgroundColor = "black")
+    );
+    square.addEventListener(
+      "mouseleave",
+      (e) => {
+        setTimeout(() => {
+          e.target.style.backgroundColor = "";
+        }, 2000);
+      },
+      false
+    );
   }
 }
 
-console.log(createSquares());
+console.log(createSquares(256));
